fd74968007bd90202f218d474dc90724a9dc474c
Purnacs <poornachandrashekar@hotmail.com>
2023-11-07 10:31:17 +0530

    file consisting of removed functions added

--- Testing/Ass4_1.py
+++ Testing/Ass4_1.py
@@ -37,27 +37,6 @@ class CNN:
             results.append(self.single_example_cnn(i))
         return torch.stack(results)
 
-def convolution(x: torch.Tensor) -> torch.Tensor:
-    results = []
-    for i in x:
-        l1 = nn.Conv2d(3,32,5)(i)
-        l1_rel = nn.ReLU()(l1)
-        l1_pool = nn.AvgPool2d(2)(l1_rel)
-        l2 = nn.Conv2d(32,64,5)(l1_pool)
-        l2_rel = nn.ReLU()(l2)
-        l2_pool = nn.AvgPool2d(2)(l2_rel)
-        l3 = nn.Conv2d(64,128,5)(l2_pool)
-        l3_rel = nn.ReLU()(l3)
-        l3_pool = nn.AvgPool2d(2)(l3_rel)
-        l4 = nn.Conv2d(128,256,5)(l3_pool)
-        l4_rel = nn.ReLU()(l4)
-        l4_pool = nn.AvgPool2d(2)(l4_rel)
-        l5 = nn.Conv2d(256,512,5)(l4_pool)
-        l5_rel = nn.ReLU()(l5)
-        result = nn.AvgPool2d(3)(l5_rel)
-        results.append(result)
-    result = torch.stack(results)
-    return result
 
 def import_data(dir_path,is_synthetic:bool = True,type:str = "train"): # import data -> NOTE: sys.argv[1] would be path to "Dataset" , so the dir_name passed should be sys.argv[1] + "/SyntheticData/images" etc
     '''
